df1aca08ff06e3df9ffb5b2d0cba5d1ecad2c4af
gloryofme <gloryofme@163.com>
2016-07-06 14:42:43 +0800

    添加service公共接口类

---
+++ onlineshop-manage/onlineshop-manage-service/src/main/java/com/gloryofme/onlineshop/manage/service/BaseService.java
@@ -0,0 +1,16 @@
+package com.gloryofme.onlineshop.manage.service;
+
+import java.util.List;
+
+/**
+ * Created by YU on 2016/7/5.
+ */
+public interface BaseService<T> {
+    T selectById(Long id);
+    T select(T t);
+    Long selectCount();
+    List<T> selectPage(int page, int rows);
+    int insert(T t);
+    int deleteById(Long id);
+    int update(T t);
+}
