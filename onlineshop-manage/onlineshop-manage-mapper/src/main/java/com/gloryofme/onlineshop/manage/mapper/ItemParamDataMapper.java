88edffe854ff7aa7d2364cb4dcf1a8f1727b0778
gloryofme <gloryofme@163.com>
2016-07-06 14:38:16 +0800

    商品参数数据Mapper

---
+++ onlineshop-manage/onlineshop-manage-mapper/src/main/java/com/gloryofme/onlineshop/manage/mapper/ItemParamDataMapper.java
@@ -0,0 +1,10 @@
+package com.gloryofme.onlineshop.manage.mapper;
+
+import com.gloryofme.onlineshop.manage.pojo.ItemParamData;
+
+/**
+ * Created by YU on 2016/7/6.
+ */
+public interface ItemParamDataMapper extends BaseMapper<ItemParamData>{
+
+}
