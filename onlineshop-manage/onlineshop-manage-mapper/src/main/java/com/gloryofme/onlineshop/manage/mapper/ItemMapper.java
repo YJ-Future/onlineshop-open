8e22015089c4d96ce7ee963be80d7265144b9583
gloryofme <gloryofme@163.com>
2016-07-06 14:37:45 +0800

    商品Mapper

--- onlineshop-manage/onlineshop-manage-mapper/src/main/java/com/gloryofme/onlineshop/manage/mapper/ItemMapper.java
+++ onlineshop-manage/onlineshop-manage-mapper/src/main/java/com/gloryofme/onlineshop/manage/mapper/ItemMapper.java
@@ -8,12 +8,5 @@ import java.util.List;
 /**商品Mapper
  * Created by YU on 2016/6/9.
  */
-public interface  ItemMapper {
-
-    Item  selectById(Long id);
-    List<Item> selectByArgs(ItemParam args);
-    int insert(Item item);
-    int deleteById(Long id);
-    int deleteByArgs(ItemParam args);
-    int update(Item item);
+public interface  ItemMapper extends BaseMapper<Item>{
 }
