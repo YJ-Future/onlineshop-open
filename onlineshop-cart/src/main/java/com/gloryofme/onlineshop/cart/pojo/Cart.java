6bbcc665e82c4c177c39746e485fb2929a62fe52
gloryofme <gloryofme@163.com>
2016-08-29 21:28:32 +0800

    Cart

---
+++ onlineshop-cart/src/main/java/com/gloryofme/onlineshop/cart/pojo/Cart.java
@@ -0,0 +1,99 @@
+package com.gloryofme.onlineshop.cart.pojo;
+
+import java.util.Date;
+
+/**
+ * @author gloryzyf<bupt_zhuyufei@163.com>
+ */
+public class Cart {
+
+    private Long id;
+
+    private Long userId;
+
+    private Long itemId;
+
+    private String itemTitle;
+
+    private String itemImage;
+
+    private Long itemPrice;
+
+    private Integer itemNum;
+
+    private Date created;
+
+    private Date updated;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Long itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemTitle() {
+        return itemTitle;
+    }
+
+    public void setItemTitle(String itemTitle) {
+        this.itemTitle = itemTitle;
+    }
+
+    public String getItemImage() {
+        return itemImage;
+    }
+
+    public void setItemImage(String itemImage) {
+        this.itemImage = itemImage;
+    }
+
+    public Long getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(Long itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public Integer getItemNum() {
+        return itemNum;
+    }
+
+    public void setItemNum(Integer itemNum) {
+        this.itemNum = itemNum;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+}
